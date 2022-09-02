3bef8d53fb115416cd6ed9b396046bd29ad95e09
wangsijie <wangsijie@silverhand.io>
2022-09-02 10:47:24 +0800

    feat: add connectors

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-    '*.ts?(x)': ['eslint --cache --fix', () => 'tsc -p tsconfig.json --noEmit'],
-    '*.scss': 'stylelint --fix',
-  };
-  
+  "*.ts?(x)": ["eslint --cache --fix", () => "tsc -p tsconfig.json --noEmit"],
+};
