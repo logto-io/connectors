04e8e56f1a535e5878b6f1347e34b266dcb9d745
Darcy Ye <darcyye@silverhand.io>
2022-11-24 10:43:56 +0800

    feat: add mock-standard-email connector

---
+++ packages/connector-mock-standard-email/src/constant.ts
@@ -0,0 +1,24 @@
+import type { ConnectorMetadata } from '@logto/connector-kit';
+
+export const defaultMetadata: ConnectorMetadata = {
+  id: 'mock-standard-email-service',
+  target: 'mock-standard-mail',
+  platform: null,
+  name: {
+    en: 'Mock Mail Service',
+    'zh-CN': 'Mock 邮件服务',
+    'tr-TR': 'Mock Mail Servis',
+    ko: 'Mock Mail Service',
+  },
+  logo: './logo.svg',
+  logoDark: null,
+  description: {
+    en: 'The description of Mock Mail Service.',
+    'zh-CN': 'Mock 邮件服务的描述。',
+    'tr-TR': 'Mock Mail Servis açıklaması.',
+    ko: 'The description of Mock SMS Service.',
+  },
+  readme: './README.md',
+  configTemplate: './docs/config-template.json',
+  isStandard: true,
+};
