40f35baa06049114b821fa47e24e7618486ed502
kentio <13550898+kentio@users.noreply.github.com>
2022-12-12 20:34:15 +0800

    fix(ses): fix desc and package name

--- packages/connector-aws-ses/src/constant.ts
+++ packages/connector-aws-ses/src/constant.ts
@@ -13,10 +13,12 @@ export const defaultMetadata: ConnectorMetadata = {
   logo: './logo.svg',
   logoDark: null,
   description: {
-    en: 'AWS provides cloud computing services to online businesses.',
-    'zh-CN': 'AWS是全球性的云服务提供商。',
-    'tr-TR': 'AWS, çevrimiçi işletmelere bulut bilişim hizmetleri sunmaktadır.',
-    ko: 'AWS는 온라인 비지니스를 위해 클라우딩 컴퓨팅 서비스를 제공합니다.',
+    en: 'Amazon SES is a cloud email service provider that can integrate into any application for bulk email sending.',
+    'zh-CN':
+      'Amazon SES 是云电子邮件发送服务提供商，它可以集成到任何应用程序中，用于批量发送电子邮件。',
+    'tr-TR':
+      'Amazon SES, toplu e-posta dağıtımı için herhangi bir uygulamaya entegre edilebilen bir bulut e-posta dağıtım hizmeti sağlayıcısıdır.',
+    ko: 'Amazon SES는 모든 애플리케이션에 통합하여 대량으로 이메일을 전송할 수 있는 클라우드 이메일 서비스 공급자입니다.',
   },
   readme: './README.md',
   configTemplate: './docs/config-template.json',
