53560d384943b95145af9fc49e884f62b3676309
{authemail@qq.com} <authemail@qq.com>
2022-12-15 19:37:56 +0800

    feat(tencent-sms): add tencent cloud sms connector

---
+++ packages/connector-tencent-sms/src/mock.ts
@@ -0,0 +1,17 @@
+export const mockedTemplateCode = 'TemplateCode';
+export const mockedConnectorConfig = {
+  accessKeyId: 'accessKeyId',
+  accessKeySecret: 'accessKeySecret',
+  signName: 'signName',
+  sdkAppId: 'sdkAppId',
+  region: 'region',
+  templates: [
+    {
+      usageType: 'SignIn',
+      templateCode: mockedTemplateCode,
+    },
+  ],
+};
+
+export const phoneTest = '13012345678';
+export const codeTest = '1234';
