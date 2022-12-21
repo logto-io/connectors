eafb8ad3937b7734437b7aba3006a04241652b4d
Gao Sun <gao@silverhand.io>
2022-12-21 19:06:39 +0800

    refactor: fix rebase errors

--- packages/connector-mock-email/jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from '@silverhand/jest-config';
-import { merge } from '@silverhand/jest-config';
-
-const config: Config.InitialOptions = merge({
-  setupFilesAfterEnv: ['jest-matcher-specific-error'],
-});
-
-export default config;
