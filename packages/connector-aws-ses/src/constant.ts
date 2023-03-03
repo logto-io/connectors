3b432cc5cdcfd22980bc67b0bdcbb1a925c538a8
Darcy Ye <darcyye@silverhand.io>
2023-03-03 14:24:57 +0800

    feat: sendgrid-mail and aws-ses form view

--- packages/connector-aws-ses/src/constant.ts
+++ packages/connector-aws-ses/src/constant.ts
@@ -1,4 +1,5 @@
 import type { ConnectorMetadata } from '@logto/connector-kit';
+import { ConnectorConfigFormItemType } from '@logto/connector-kit';
 
 export const defaultMetadata: ConnectorMetadata = {
   id: 'aws-ses-mail',
@@ -21,5 +22,95 @@ export const defaultMetadata: ConnectorMetadata = {
     ko: 'Amazon SES는 모든 애플리케이션에 통합하여 대량으로 이메일을 전송할 수 있는 클라우드 이메일 서비스 공급자입니다.',
   },
   readme: './README.md',
-  configTemplate: './docs/config-template.json',
+  formItems: [
+    {
+      key: 'accessKeyId',
+      label: 'Access Key ID',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<access-key-id>',
+    },
+    {
+      key: 'accessKeySecret',
+      label: 'Access Key Secret',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<access-key-secret>',
+    },
+    {
+      key: 'region',
+      label: 'Region',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<region>',
+    },
+    {
+      key: 'emailAddress',
+      label: 'Email Address',
+      type: ConnectorConfigFormItemType.Text,
+      required: false,
+      placeholder: '<email-address>',
+    },
+    {
+      key: 'emailAddressIdentityArn',
+      label: 'Email Address Identity ARN',
+      type: ConnectorConfigFormItemType.Text,
+      required: false,
+      placeholder: '<email-address-identity-arn>',
+    },
+    {
+      key: 'feedbackForwardingEmailAddress',
+      label: 'Feedback Forwarding Email Address',
+      type: ConnectorConfigFormItemType.Text,
+      required: false,
+      placeholder: '<feedback-forwarding-email-address>',
+    },
+    {
+      key: 'feedbackForwardingEmailAddressIdentityArn',
+      label: 'Feedback Forwarding Email Address Identity ARN',
+      type: ConnectorConfigFormItemType.Text,
+      required: false,
+      placeholder: '<feedback-forwarding-email-address-identity-arn>',
+    },
+    {
+      key: 'configurationSetName',
+      label: 'Configuration Set Name',
+      type: ConnectorConfigFormItemType.Text,
+      required: false,
+      placeholder: '<configuration-set-name>',
+    },
+    {
+      key: 'templates',
+      label: 'Templates',
+      type: ConnectorConfigFormItemType.Json,
+      required: true,
+      defaultValue: [
+        {
+          usageType: 'SignIn',
+          subject: '<sign-in-template-subject>',
+          content: '<sign-in-template-content>',
+        },
+        {
+          usageType: 'Register',
+          subject: '<register-template-subject>',
+          content: '<register-template-content>',
+        },
+        {
+          usageType: 'ForgotPassword',
+          subject: '<forgot-password-template-subject>',
+          content: '<forgot-password-template-content>',
+        },
+        {
+          usageType: 'Generic',
+          subject: '<generic-template-subject>',
+          content: '<generic-template-content>',
+        },
+        {
+          usageType: 'Test',
+          subject: '<test-template-subject>',
+          content: '<test-template-content>',
+        },
+      ],
+    },
+  ],
 };
