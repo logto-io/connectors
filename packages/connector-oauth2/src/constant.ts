9b1f91be3fa28478d0565f9523411a916d1159f4
Darcy Ye <darcyye@silverhand.io>
2023-03-06 12:31:05 +0800

    feat: oauth form view (#124)

--- packages/connector-oauth2/src/constant.ts
+++ packages/connector-oauth2/src/constant.ts
@@ -1,5 +1,5 @@
 import type { ConnectorMetadata } from '@logto/connector-kit';
-import { ConnectorPlatform } from '@logto/connector-kit';
+import { ConnectorConfigFormItemType, ConnectorPlatform } from '@logto/connector-kit';
 
 export const defaultMetadata: ConnectorMetadata = {
   id: 'oauth2',
@@ -16,8 +16,85 @@ export const defaultMetadata: ConnectorMetadata = {
     'zh-CN': 'OAuth 2.0 授权框架是的第三方应用能够有权限访问 HTTP 服务。',
   },
   readme: './README.md',
-  configTemplate: './docs/config-template.json',
   isStandard: true,
+  formItems: [
+    {
+      key: 'authorizationEndpoint',
+      label: 'Authorization Endpoint',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<authorization-endpoint>',
+    },
+    {
+      key: 'tokenEndpoint',
+      label: 'Token Endpoint',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<token-endpoint>',
+    },
+    {
+      key: 'userInfoEndpoint',
+      label: 'User Info Endpoint',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<user-info-endpoint>',
+    },
+    {
+      key: 'clientId',
+      label: 'Client ID',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<client-id>',
+    },
+    {
+      key: 'clientSecret',
+      label: 'Client Secret',
+      type: ConnectorConfigFormItemType.Text,
+      required: true,
+      placeholder: '<client-secret>',
+    },
+    {
+      key: 'tokenEndpointResponseType',
+      label: 'Token Endpoint Response Type',
+      type: ConnectorConfigFormItemType.Select,
+      selectItems: [
+        {
+          title: 'query-string',
+          value: 'query-string',
+        },
+        { title: 'json', value: 'json' },
+      ],
+      required: false,
+      defaultValue: 'query-string',
+    },
+    {
+      key: 'scope',
+      label: 'Scope',
+      type: ConnectorConfigFormItemType.Text,
+      required: false,
+      placeholder: '<space-delimited-scope>',
+    },
+    {
+      key: 'profileMap',
+      label: 'Profile Map',
+      type: ConnectorConfigFormItemType.Json,
+      required: false,
+      defaultValue: {
+        id: 'user_id',
+        email: 'email_verified',
+        phone: 'phone_verified',
+        name: 'full_name',
+        avatar: 'avatar_url',
+      },
+    },
+    {
+      key: 'customConfig',
+      label: 'Custom Config',
+      type: ConnectorConfigFormItemType.Json,
+      required: false,
+      defaultValue: {},
+    },
+  ],
 };
 
 export const defaultTimeout = 5000;
